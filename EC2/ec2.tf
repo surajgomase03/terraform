73031dcf003268bc65f7ee7bbd93fc960e3d02b3
Suraj Gomase <surajgomase303@gmail.com>
2025-04-16 00:23:22 +0530

    EC2 metadata argument added

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -37,10 +37,13 @@ resource "aws_security_group" "first_sg" {
 
 
 resource "aws_instance" "first_ec2" {
+  count = 2 #meta argumen, multiple resources
   ami = var.ami_id
   key_name = aws_key_pair.first_key.key_name
   security_groups = [aws_security_group.first_sg]
   instance_type = "t2.micro"
+  user_data = file("install.sh")
+
 
   root_block_device {
     volume_size = 15
@@ -50,4 +53,13 @@ resource "aws_instance" "first_ec2" {
     Name = "${var.env}-ec2"
     Env = var.env
   }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.first_ec2[*].public_ip
+  
+}
+output "aws_ec2_dns" {
+  value = aws_instance.first_ec2[*].public_dns
+  
 }
